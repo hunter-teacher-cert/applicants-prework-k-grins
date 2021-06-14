55f1acc03ac2ce356e6302d26d0dc9bd68869cb0
Kevin Claesgens <kevgens@gmail.com>
2021-06-13 21:23:54 -0400

    Just trying to add this file to pre02

---
+++ pre02/Date.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+	public static void main(String[] args){
+		String day = "Saturday";
+		int date = 12;
+		String month = "June";
+		int year = 2021;
+	System.out.println(day + ", " + month + " " + date + ", " + year);
+		
+		
+	}
+}
